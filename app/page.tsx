83397ee592c23405363a8e09d24be36d66b0b776
Dilkash Maryam <160109487+DilkashMaryam@users.noreply.github.com>
2024-10-21 01:10:19 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
 import Footer from "./components/Footer/Footer";
+import Deals from "./components/Deals/page";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
       <HeroSection></HeroSection>
+      <Deals/>
       <Footer></Footer>
     </div>
   );

60e91f9532568500eec9bbb6a601734cec33fff8
Dilkash Maryam <160109487+DilkashMaryam@users.noreply.github.com>
2024-10-21 01:11:17 +0500

    Update HeroSection.tsx

--- app/components/HeroSection/HeroSection.tsx
+++ app/components/HeroSection/HeroSection.tsx
@@ -12,6 +12,26 @@ const HeroSection = () => {
       height={900}
       className='object-cover'
       />
+      <div className="flex flex-col sm:flex-row item-center justify-between border-b border-gray-400 px-5 py-5"></div>
+<div className="flex flex-col sm:flex-row item-center justify-between border-b border-gray-400 px-5 py-5 mt-10">
+        <ul className='inline-flex flex-col sm:flex-row space-y-5 sm:space-y-0 sm:space-x-8'>
+       <FaRocket className='size-10 text-green-500'/>
+       <li className='text-black font-bold'>Home Delivery
+        <p className='text-gray-500 font-thin'>Delivery service in all citites</p></li>
+       <FaRecycle className='size-10 text-green-500'/>
+       <li className='text-black font-bold'>Immediate Return
+        <p className='text-gray-500 font-thin'>If lants have problem</p></li>
+       <FaWallet className='size-10 text-green-500'/>
+       <li className='text-black font-bold'>Easypaia Payment 
+        <p className='text-gray-500 font-thin'>COD or Easypaisa payment</p></li>
+       <FaGift className='size-10 text-green-500'/>
+       <li className='text-black font-bold'>Site Visit 
+        <p className='text-gray-500 font-thin'>Paid 0n-demand visit</p></li>
+       <FaMessage className='size-10 text-green-500'/>
+       <li className='text-black font-bold'>24/7 Support 
+        <p className='text-gray-500 font-thin'>Dedicated support</p></li>
+        </ul>
+        </div>
     </div>
   )
 }
